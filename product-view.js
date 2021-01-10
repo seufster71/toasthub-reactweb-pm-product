7eebcd84f385254cf678fcd2239ca2f7d1ed7aca
Edward Seufert <edwardseufert@Edwards-MBP.home>
2021-01-10 16:59:43 -0500

    cleanup

--- product-view.js
+++ product-view.js
@@ -7,7 +7,7 @@ import Input from '../../coreView/common/text-input';
 import Select from '../../coreView/common/select-input';
 
 
-export default function PMProductView({containerState, itemState, appPrefs, onListLimitChange,
+export default function PMProductView({itemState, appPrefs, onListLimitChange,
 	onSearchChange, onSearchClick, onPaginationClick, onOrderBy, onOption, 
 	closeModal, inputChange, session}) {
 
@@ -23,8 +23,8 @@ export default function PMProductView({containerState, itemState, appPrefs, onLi
 	}
 	
 	let deleteModalHeader = "Delete ";
-	if (containerState.selected != null && containerState.selected.name != null) {
-		deleteModalHeader += containerState.selected.name;
+	if (itemState.selected != null && itemState.selected.name != null) {
+		deleteModalHeader += itemState.selected.name;
 	}
 	
 	let viewPortSmall = false;
@@ -34,12 +34,8 @@ export default function PMProductView({containerState, itemState, appPrefs, onLi
     	<div>
     		{viewPortSmall ? (
     			<ListBuilder
-		  	      	containerState={containerState}
+		  	      	itemState={itemState}
 		  	      	header={header}
-		  	      	items={itemState.items}
-		  	      	itemCount={itemState.itemCount}
-		  	      	listStart={itemState.listStart}
-		  	      	listLimit={itemState.listLimit}
 		  	     	columns={columns}
 		  	      	appPrefs={appPrefs}
 		  	      	onListLimitChange={onListLimitChange}
@@ -48,17 +44,11 @@ export default function PMProductView({containerState, itemState, appPrefs, onLi
 		  	      	onPaginationClick={onPaginationClick}
 		  			onOrderBy={onOrderBy}
 	  				onOption={onOption}
-		  			orderCriteria={itemState.orderCriteria}
-	  				searchCriteria={itemState.searchCriteria}
 		  	      />
     		) : (
 	    		<Table
-	    			containerState={containerState}
+	    			itemState={itemState}
 	    			header={header}
-	    			items={itemState.items}
-	    			itemCount={itemState.itemCount}
-	    			listStart={itemState.listStart}
-	    			listLimit={itemState.listLimit}
 	    			columns={columns}
 	    			labelGroup = {group}
 	    			appPrefs={appPrefs}
@@ -68,11 +58,9 @@ export default function PMProductView({containerState, itemState, appPrefs, onLi
 	    			onPaginationClick={onPaginationClick}
 	    			onOrderBy={onOrderBy}
 	    			onOption={onOption}
-	    			orderCriteria={itemState.orderCriteria}
-					searchCriteria={itemState.searchCriteria}
 	    		/>
     		)}
-    		<Modal isOpen={containerState.isDeleteModalOpen} onClose={() => closeModal()} >
+    		<Modal isOpen={itemState.isDeleteModalOpen} onClose={() => closeModal()} >
     			<div className="modal-dialog">
     				<div className="modal-content">
     					<div className="modal-header">
@@ -83,7 +71,7 @@ export default function PMProductView({containerState, itemState, appPrefs, onLi
     						<h3>Are you sure you want to delete?</h3>
     					</div>
     					<div className="modal-footer">
-    						<button type="button" className="btn btn-primary" onClick={() => onOption("DELETEFINAL",containerState.selected)}>Delete</button>
+    						<button type="button" className="btn btn-primary" onClick={() => onOption("DELETEFINAL",itemState.selected)}>Delete</button>
     						<button type="button" className="btn btn-secondary" data-dismiss="modal" onClick={() => closeModal()}>Close</button>
     					</div>
     				</div>
@@ -95,8 +83,7 @@ export default function PMProductView({containerState, itemState, appPrefs, onLi
 
 
 PMProductView.propTypes = {
-  containerState: PropTypes.object,
-  itemState: PropTypes.object,
+  itemState: PropTypes.object.isRequired,
   appPrefs: PropTypes.object,
   onListLimitChange: PropTypes.func,
   onSearchChange: PropTypes.func,
